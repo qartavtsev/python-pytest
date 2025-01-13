823e5264e7c58ee14dbe43a5fc9495c598f139b5
LaenorSama <laenorsama@yandex.ru>
2025-01-13 11:36:26 +0500

    add logo

--- test_dnd_classes.py
+++ test_dnd_classes.py
@@ -16,6 +16,12 @@ ERROR_TYPES = [AssertionError, ValueError, TypeError, KeyError]
 def test_take_true_damage(damage, expected):
     # добавляем первый шаг, создание персонажа и проверяем что он создался
     with allure.step('Шаг1: создание персонажа'):
+        with open("img/logo.jpeg", "rb") as image_file:
+            allure.attach(
+                image_file.read(),
+                name="ТестОпс Лого",
+                attachment_type=allure.attachment_type.JPEG
+            )
         alex_person = Person('Alex')
         assert alex_person.name == 'Alex'
 
@@ -32,12 +38,13 @@ def test_take_true_damage(damage, expected):
         with allure.step("Ожидаемый результат"):
             expected_result = 5
             assert result == expected_result, f"Ожидали: {expected_result}, получили: {result}"
+    with allure.step("Шаг 4: Шаг со случайной ошибкой"):
+        # Вставляем случайную ошибку, это приведет к сломанному тесту
+        if random.random() < 0.2:  # 20% шанс на ошибку
+            error_type = random.choice(ERROR_TYPES)  # случайно выбираем тип ошибки
+            raise error_type(f"Случайная ошибка: {error_type.__name__}")
 
-    # Вставляем случайную ошибку, это приведет к сломанному тесту
-    if random.random() < 0.2:  # 20% шанс на ошибку
-        error_type = random.choice(ERROR_TYPES)  # случайно выбираем тип ошибки
-        raise error_type(f"Случайная ошибка: {error_type.__name__}")
-
-    else:
-        alex_person.take_true_damage(damage)
-        assert alex_person.hp == expected, f"Ожидалось {expected}, но получено {alex_person.hp}"
+        else:
+            with allure.step("Шаг 5: проверка реальности"):
+                alex_person.take_true_damage(damage)
+                assert alex_person.hp == expected, f"Ожидалось {expected}, но получено {alex_person.hp}"
