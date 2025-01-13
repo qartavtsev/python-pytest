4b7c5020a04629066a4b3fcfd749feb52b452bcd
LaenorSama <laenorsama@yandex.ru>
2025-01-13 11:41:54 +0500

    add logo

--- test_dnd_classes.py
+++ test_dnd_classes.py
@@ -6,16 +6,16 @@ from dnd_classes import Person
 # Создадим список типов ошибок, которые могут быть случайно выбраны
 ERROR_TYPES = [AssertionError, ValueError, TypeError, KeyError]
 
-@pytest.mark.parametrize("damage, expected", [
+@pytest.mark.parametrize('damage, expected', [
     (1, 9),
     (2, 8),
     (3, 7),
     (4, 8), # 4th test already failed
 ])
-@allure.step("Шаг: тест")
+#@allure.step("Шаг: тест")
 def test_take_true_damage(damage, expected):
     # добавляем первый шаг, создание персонажа и проверяем что он создался
-    with allure.step('Шаг1: создание персонажа'):
+    with allure.step('Шаг1: Создание персонажа'):
         with open("img/logo.jpeg", "rb") as image_file:
             allure.attach(
                 image_file.read(),
@@ -31,20 +31,20 @@ def test_take_true_damage(damage, expected):
         assert alex_person.hp == 10
 
     # Добавляем шаг 3, с ожидаемым результатом
-    with allure.step("Шаг 3: Выполняем действие"):
-        with allure.step("Выполняем действие деление"):
+    with allure.step('Шаг 3.1: Выполняем действие'):
+        with allure.step('Выполняем действие деление'):
             result = 10 / 2
 
-        with allure.step("Ожидаемый результат"):
+        with allure.step('Шаг 3.2: Проверяем действие'):
             expected_result = 5
-            assert result == expected_result, f"Ожидали: {expected_result}, получили: {result}"
-    with allure.step("Шаг 4: Шаг со случайной ошибкой"):
+            assert result == expected_result, f'Ожидали: {expected_result}, получили: {result}'
+    with allure.step('Шаг 4: Шаг со случайной ошибкой'):
         # Вставляем случайную ошибку, это приведет к сломанному тесту
         if random.random() < 0.2:  # 20% шанс на ошибку
             error_type = random.choice(ERROR_TYPES)  # случайно выбираем тип ошибки
-            raise error_type(f"Случайная ошибка: {error_type.__name__}")
+            raise error_type(f'Случайная ошибка: {error_type.__name__}')
 
         else:
-            with allure.step("Шаг 5: проверка реальности"):
+            with allure.step('Шаг 5: проверка реальности'):
                 alex_person.take_true_damage(damage)
-                assert alex_person.hp == expected, f"Ожидалось {expected}, но получено {alex_person.hp}"
+                assert alex_person.hp == expected, f'Ожидалось {expected}, но получено {alex_person.hp}'
