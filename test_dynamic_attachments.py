634a1ab31dfed1b4784a10bbec72a0d18063c937
LaenorSama <laenorsama@yandex.ru>
2025-01-14 11:52:17 +0500

    add test_dynamic_attacments.py

--- test_dynamic_attachments.py
+++ test_dynamic_attachments.py
@@ -11,21 +11,21 @@ import random
 @allure.feature("BackEnd")
 @allure.story("Server")
 @pytest.mark.parametrize('parametr1, parametr2', [
-    ('Car', 2),
-    ('Bicycle', 4)
+    ('Car', 4),
+    ('Bike', 2)
         ])
 def test_add_dynamic_att(parametr1, parametr2):
     with allure.step('Шаг 1: Создание вложения картинки.'):
         allure.attach.file(os.path.join('img', 'logo.jpeg'), name='JPG example', attachment_type=allure.attachment_type.JPG)
 
     with allure.step('Шаг 2: Создание вложения HTML.'):
-        allure.attach(f'<h1>Example html attachment</h1> <h2>У {parametr1} {parametr2} колеса.</h2>', name='HTML example', attachment_type=allure.attachment_type.HTML  )
+        allure.attach(f'<h1>The {parametr1} has {parametr2} wheels.</h1>', name='HTML example', attachment_type=allure.attachment_type.HTML  )
 
     with allure.step('Шаг 3: Создание вложения TXT.'):
-        allure.attach(f'У {parametr1} {parametr2} колеса.', name='TXT example', attachment_type=allure.attachment_type.TEXT)
+        allure.attach(f'The {parametr1} has {parametr2} wheels.', name='TXT example', attachment_type=allure.attachment_type.TEXT)
 
     with allure.step('Шаг 4: Создание вложения CSV.'):
-        allure.attach(f'тип транспорта,количество колес\n{parametr1},{parametr2}', name='CSV example', attachment_type=allure.attachment_type.CSV)
+        allure.attach(f'vehicle type,wheels count\n{parametr1},{parametr2}', name='CSV example', attachment_type=allure.attachment_type.CSV)
 
     with allure.step('Шаг 5: Создание вложения JSON.'):
         allure.attach(json.dumps({'vehicle': parametr1, 'wheels': parametr2}, indent=2), name='JSON example', attachment_type=allure.attachment_type.JSON)
