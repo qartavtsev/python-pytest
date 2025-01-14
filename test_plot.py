8405bd7e745f9450f8445f874f186ffac082ebe2
LaenorSama <laenorsama@yandex.ru>
2025-01-14 15:49:44 +0500

    add test_plot_gerenation

--- test_plot.py
+++ test_plot.py
@@ -10,7 +10,7 @@ import pytest
 @allure.epic('TestOps')
 @allure.feature('BackEnd')
 @allure.story('Server')
-@allure.description('Этот тест проверяет построение разных графиков.')
+#@allure.description('Этот тест проверяет построение разных графиков.')
 @allure.description_html('<h1>Этот тест проверяет построение разных графиков.</h1'>)
 @allure.link('https://ru.wikipedia.org/wiki/Тригонометрические_функции', name='Тригонометрические_функции')
 @pytest.mark.parametrize('fucn_name,func_deal', [
