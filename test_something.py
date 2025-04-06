97539858a3ef876141ad7b143f307eaae1667984
LaenorSama <laenorsama@yandex.ru>
2025-04-06 11:04:39 +0300

    rename test_run_one_manual_test

--- test_something.py
+++ test_something.py
@@ -5,7 +5,7 @@ import pytest
 @allure.title('Запуск одного ручного теста.')
 @allure.manual(True)
 @allure.id("431")
-def test_test_test():
+def test_run_one_manual_test():
     with allure.step("Перейти в раздел тест-кейсы."):
         pass
     with allure.step("Найти ручной тест из таблицы."):
@@ -31,26 +31,3 @@ def test_test_test():
     with allure.step("Найти в списке запусков только что созданный запуск."):
         pass
 
-@allure.manual(True)
-def test_02():
-    with allure.step("Демо на гейзенбаг."):
-        pass
-    pass
-
-
-@allure.id("808")
-@allure.title("Проверка переключения на темную тему")
-@allure.label("owner", "Alex")
-def test_method():
-    with allure.step("Зайти на портал"):
-        pass
-    with allure.step("Перейти в меню пользователя"):
-        pass
-    with allure.step("Открыть настройки оформления"):
-        pass
-    with allure.step("Переключить тему оформления на темную"):
-        pass
-    with allure.step("Убедиться что интерфейс переключился на темную тему"):
-        pass
-    with allure.step("Убедиться что интерфейс переключился на темную тему_тест 2"):
-        pass
