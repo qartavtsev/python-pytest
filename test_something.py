8bca3587ce4ef37ad8c14d4b060286bcf8f524db
LaenorSama <laenorsama@yandex.ru>
2025-04-09 13:39:27 +0500

    test

--- test_something.py
+++ test_something.py
@@ -31,17 +31,11 @@ def test_test_test():
     with allure.step("Найти в списке запусков только что созданный запуск."):
         pass
 
-@allure.id("703")
-def test_dark_mode():
-    with allure.step("Зайти на портал"):
-        with allure.step("Открыть новую вкладку в режиме Incognito."):
-            pass
-    with allure.step("Ввести в адресную строку адрес портала. Нажать Enter."):
-        with allure.step("Expected Result"):
-            with allure.step("Открылась страница авторизации на портале"):
-                pass
-    with allure.step("улыбнуться"):
-        pass
+@allure.id("817")
+@allure.title("Проверка переключения на темную тему")
+@allure.label("owner", "Alex")
+def test_method():
+    sharedStep144()
     with allure.step("Перейти в меню пользователя"):
         pass
     with allure.step("Открыть настройки оформления"):
@@ -49,4 +43,19 @@ def test_dark_mode():
     with allure.step("Переключить тему оформления на темную"):
         pass
     with allure.step("Убедиться что интерфейс переключился на темную тему"):
+        with allure.step("Expected Result"):
+            with allure.step("результат 1"):
+                pass
+
+
+@allure.step("Зайти на портал")
+def sharedStep144():
+    with allure.step("Открыть новую вкладку в режиме Incognito."):
         pass
+    with allure.step("Ввести в адресную строку адрес портала. Нажать Enter."):
+        with allure.step("Expected Result"):
+            with allure.step("Открылась страница авторизации на портале"):
+                pass
+            with allure.step("Attachment [35]"):
+                pass
+
