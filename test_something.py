9a1a618b2f7d584f4df941265876a84bef321f3c
LaenorSama <laenorsama@yandex.ru>
2025-04-10 14:50:59 +0500

    test

--- test_something.py
+++ test_something.py
@@ -31,3 +31,31 @@ def test_run_one_manual_test():
     with allure.step("Найти в списке запусков только что созданный запуск."):
         pass
 
+
+@allure.id("836")
+@allure.title("Проверка переключения на темную тему")
+@allure.label("owner", "Alex")
+def test_method():
+    Allure::label("Ytracker", "DEMO-11");
+    sharedStep144()
+    with allure.step("Перейти в меню пользователя"):
+        pass
+    with allure.step("Открыть настройки оформления"):
+        pass
+    with allure.step("Переключить тему оформления на темную"):
+        with allure.step("Убедиться что интерфейс переключился на темную тему"):
+            pass
+
+
+@allure.step("Зайти на портал")
+def sharedStep144():
+    with allure.step("Открыть новую вкладку в режиме Incognito."):
+        pass
+    with allure.step("Ввести в адресную строку адрес портала. Нажать Enter."):
+        with allure.step("Expected Result"):
+            with allure.step("Открылась страница авторизации на портале"):
+                pass
+            with allure.step("Attachment [35]"):
+                pass
+
+
